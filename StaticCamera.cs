615644443e832b06cfda82ac5da5ecd2634ba891
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-04-29 01:46:35 +0200

    remove fire

--- StaticCamera.cs
+++ StaticCamera.cs
@@ -17,38 +17,15 @@ using Unity.Collections;
 namespace PlateUp_StaticCamera
 {
     public class StaticCamera : GenericSystemBase, IModSystem
-    {
-
-        private EntityQuery Appliances;
-
-        struct CHasBeenSetOnFire : IModComponent
-        {
-
-        }
+    {   
 
         protected override void Initialise()
         {
-            base.Initialise();
-
-            Appliances = GetEntityQuery(new QueryHelper()
-                   .All(typeof(CAppliance))
-                   .None(
-                       typeof(CFire),
-                       typeof(CIsOnFire),
-                       typeof(CFireImmune),
-                       typeof(CHasBeenSetOnFire)
-                   ));
+            base.Initialise();            
         }
 
         protected override void OnUpdate()
-        {
-            var appliances = Appliances.ToEntityArray(Allocator.TempJob);
-            foreach (var appliance in appliances)
-            {
-                EntityManager.AddComponent<CIsOnFire>(appliance);
-                EntityManager.AddComponent<CHasBeenSetOnFire>(appliance);
-            }
-            appliances.Dispose();
+        {            
         }
     }
 }
