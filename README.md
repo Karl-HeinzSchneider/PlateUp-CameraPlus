e970423b558e6baa8277abd6aa4e927c181a9ba8
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2024-04-28 19:56:26 +0200

    Init Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# PlateUp-StaticCamera
+ 
---
+++ StaticCamera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cinemachine;
+using Kitchen;
+using KitchenMods;
+
+using UnityEngine;
+//using UnityEngine.InputSystem;
+using Unity.Entities;
+
+
+namespace PlateUp_StaticCamera
+{
+    public class StaticCamera : GenericSystemBase, IModSystem
+    {
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+        }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+    }
+}
